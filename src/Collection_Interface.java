c83200032c0ee332fb9c228051c980c8df955bd2
Samuel Cardonis <o528060707@gmail.com>
2018-12-19 08:27:29 +0200

    comment

--- src/Collection_Interface.java
+++ src/Collection_Interface.java
@@ -3,10 +3,10 @@
  */
 public interface Collection_Interface {
     //public void initialize();//solution should be in O(?)
-    public void add(int x);          //solution should be in O(1)
-    public void remove(int x);          //solution should be in O(1)
-    public int getRandom(int randomIndex);          //solution should be in O(1)
-    public Integer contains(int x);  //solution should be in O(1)
+    public void add(int x);               //solution should be in O(1)
+    public void remove(int x);            //solution should be in O(1)
+    public int getRandom(int randomIndex);//solution should be in O(1)
+    public Integer contains(int x);       //solution should be in O(1)
 
 
 
